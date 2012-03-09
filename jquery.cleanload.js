35280447e4f32ef7132b8d42b248a0448c30a7ba
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2012-03-09 03:36:26 -0500

    basic functionality. no options or completion callback

--- jquery.cleanload.js
+++ jquery.cleanload.js
@@ -4,6 +4,15 @@
 
         }, options);
 
-    }
+        return this.each(function () {
+            var $this = $(this);
+            $this.css( 'visibility', 'hidden' )
+                 .attr( 'src', $this.attr('data-src') )
+                 .load(function () {
+                     $(this).css( {visibility: 'visible', opacity: 0} )
+                        .animate( {opacity: 1}, 700 );
+                 });
 
+        });
+    }
 })(jQuery);
