de07706e3304f10326a76dad1fbeda421751e482
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2012-03-09 04:36:49 -0500

    added completion callback and animation options, added a README

--- jquery.cleanload.js
+++ jquery.cleanload.js
@@ -1,18 +1,38 @@
+/* Fade in images after they are fully loaded.
+ *   Images must have their intended src attribute set as attribute data-src
+ *      and not have a src attr.
+ *
+ *   Takes an animations options object with the properties defined here:
+ *   http://api.jquery.com/animate/
+ *
+ *   Usage Example:
+ *     $('img').cleanLoad({
+ *                      duration: 'fast',
+ *                      easing: 'swing',
+ *                      complete: function (){ console.log("complete."); }
+ *                   });
+ *
+ */
 (function ($) {
-    $.fn.cleanLoad = function ( options ) {
-        var settings = $.extend({
+    $.fn.cleanLoad = function ( animate_options ) {
 
-        }, options);
+        /* lambda, used as animate's callback. Calls user's
+         *   "complete" callback when we're all done. */
+        var self = this, loaded = 0;
+        self.all_complete = animate_options.complete;
+        animate_options.complete = function () {
+            loaded++;
+            if (loaded == self.length) self.all_complete(this);
+        };
 
         return this.each(function () {
-            var $this = $(this);
-            $this.css( 'visibility', 'hidden' )
-                 .attr( 'src', $this.attr('data-src') )
+            $(this).css( 'visibility', 'hidden' )
+                 .attr( 'src', $(this).attr('data-src') )
                  .load(function () {
                      $(this).css( {visibility: 'visible', opacity: 0} )
-                        .animate( {opacity: 1}, 700 );
+                        .animate( {opacity: 1}, animate_options );
                  });
 
         });
-    }
+    };
 })(jQuery);
